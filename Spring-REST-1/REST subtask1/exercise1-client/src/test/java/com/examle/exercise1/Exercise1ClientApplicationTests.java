2cfe6d3a10e8dd5b25db439a4a08efe0fa055c8b
Megharaj77 <101635020+Megharaj77@users.noreply.github.com>
2022-04-11 15:51:20 +0530

    Add files via upload

---
+++ Spring-REST-1/REST subtask1/exercise1-client/src/test/java/com/examle/exercise1/Exercise1ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examle.exercise1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise1ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
