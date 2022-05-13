a2dd48a79e976dd4dedaf3952eb217ab56138c22
Megharaj77 <101635020+Megharaj77@users.noreply.github.com>
2022-05-13 09:43:25 +0530

    Add files via upload

---
+++ subtask1/SpringMvcSimpleApp/src/main/java/com/skillsoft/controller/WebServletConfiguration.java
@@ -0,0 +1,38 @@
+package com.skillsoft.controller;
+
+import javax.servlet.ServletContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class WebServletConfiguration implements WebApplicationInitializer{
+	
+
+	public void onStartup(ServletContext ctx)throws ServletException{
+		
+//		AnnotationConfigWebApplicationContext appContext=new AnnotationConfigWebApplicationContext();
+//		appContext.register(ApplicationContextConfiguration.class);
+//		
+//		ServletRegistration.Dynamic servlet=ctx.addServlet("dispatcher",new DispatcherServlet(appContext));
+		
+		
+		
+		
+		/*2nd project*/
+		
+		
+		XmlWebApplicationContext appContext=new XmlWebApplicationContext();
+		appContext.setConfigLocation("/WEB-INF/application-context.xml");
+		
+		ServletRegistration.Dynamic servlet=ctx.addServlet("dispatcher", new DispatcherServlet(appContext));
+		
+		servlet.setLoadOnStartup(1);
+		servlet.addMapping("/");
+	}
+
+}
---
+++ subtask1/SpringMvcSimpleApp/src/main/webapp/home.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Spring MVC Page</title>
+</head>
+<body>
+<h1>Welcome to Spring MVC</h1>
+<span>Using application context XML controller...</span>
+</body>
+</html>
